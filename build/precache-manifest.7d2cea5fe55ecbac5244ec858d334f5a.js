d71d3cd5a0d75b00d26bed428b55f925fbb1a180
Manish Sahani <manishsahani1351111@gmail.com>
2020-04-26 18:51:35 +0530

    static build added

---
+++ build/precache-manifest.7d2cea5fe55ecbac5244ec858d334f5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e8bf9d9a5a60e8d3aa39cd166f0880",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f594f47db2aac82b72ab",
+    "url": "/static/css/main.caf3607c.chunk.css"
+  },
+  {
+    "revision": "d32c38127d141150f441",
+    "url": "/static/js/2.40a5a710.chunk.js"
+  },
+  {
+    "revision": "39636149ac99ca93f38f9066a06f3b1b",
+    "url": "/static/js/2.40a5a710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f594f47db2aac82b72ab",
+    "url": "/static/js/main.347598ba.chunk.js"
+  },
+  {
+    "revision": "ac1bc62a50626926886b",
+    "url": "/static/js/runtime-main.d2ff8073.js"
+  },
+  {
+    "revision": "9ea8ee4735f9d3f65abf7c3948aa61f4",
+    "url": "/static/media/pablo-done.9ea8ee47.png"
+  }
+]);
